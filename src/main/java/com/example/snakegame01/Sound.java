adcb54981d35c72dafd77150cc6e90b28fdf3fc0
Florentina Hajdini <florentina.hajdini@stud.fh-campuswien.ac.at>
2023-01-22 17:42:12 +0100

    Sound class new

--- src/main/java/com/example/snakegame01/Sound.java
+++ src/main/java/com/example/snakegame01/Sound.java
@@ -1,39 +1,47 @@
 package com.example.snakegame01;
 
-import javafx.scene.layout.AnchorPane;
-
 import javax.sound.sampled.*;
-import java.io.File;
+import java.net.URL;
+
 public class Sound {
     public static Sound playSound;
-    private static App clip;
-    private Clip music;
     public static Sound ifEats;
+    public static Clip music;
+    static boolean check;
+    static URL[] File = new URL[2];
 
+    public Sound() {
+        File[0] = getClass().getResource("soundSnakeGame.wav");
+        File[1] = getClass().getResource("EATING.wav");
 
-    public static void playSound() throws Exception {
-        try {
-            Clip clip = AudioSystem.getClip();
-            AudioInputStream audioInputStream =
-                    AudioSystem.getAudioInputStream(new File("C:\\Users\\flori\\IdeaProjects\\SnakeGame01\\src\\main\\resources\\sound snake game.wav"));
-            ((Clip) clip).open(audioInputStream);
-            clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
-    public static void ifEats() throws Exception{
-        try {
-            Clip clipEats = AudioSystem.getClip();
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\flori\\IdeaProjects\\SnakeGame01\\src\\main\\resources\\EATING.wav"));
-            ((Clip)clipEats).open(audioInputStream);
-            clipEats.start();
+
+    public static void playSound(){
+        try{
+            //Java format to open sound file
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(File[0]);
+            music = AudioSystem.getClip();
+            music.open(audioInputStream);
+            if(check){
+                music.start();
+                music.loop(Clip.LOOP_CONTINUOUSLY);
+            } else music.stop();
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
-}
+        public static void ifEats(){
+            try {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(File[1]);
+                Clip clipEats = AudioSystem.getClip();
+                clipEats.open(audioInputStream);
+                clipEats.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 // Youtube: https://youtu.be/5_89GjL5AhY
 
